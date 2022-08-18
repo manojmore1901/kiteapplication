5167752d2279f3d4d89de15ffd038e875fdf9677
unknown <aadarshgaikwad111@gmail.com>
2022-08-18 17:42:51 +0530

    changes made

--- src/main/java/allpackages/Kite_Login_Page.java
+++ src/main/java/allpackages/Kite_Login_Page.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//hi Manoj
 public class Kite_Login_Page {
 	@FindBy(id="userid") private WebElement user_id;
 	@FindBy(id="password") private WebElement pwd;
